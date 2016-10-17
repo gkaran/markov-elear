87acf7745c2fb8e25896163c3bb5a7506a9f2e3f
argy <krouzos@gmail.com>
2016-10-17 22:00:41 +0300

    phase #1

--- src/markov_elear/generator.clj
+++ src/markov_elear/generator.clj
@@ -1,11 +1,22 @@
-(ns markov-elear.generator)
+(ns markov-elear.generator
+  (:require [clojure.string :refer [split]])
+  (:require [clojure.set :refer [union]])
+  (:require [clojure.core :refer [partition-all subvec merge-with]]))
 
-(defn text->words [text])
+(defn text->words [text] (split text #"\s+"))
 
-(defn words->transitions [words])
+(defn words->transitions [words] (partition-all 3 1 words))
 
-(defn fmt-trans [trans])
+(defn fmt-trans [[a b c]] {[a b] (if (= c nil) #{} #{c} )})
 
-(defn word-chain [word-transitions])
+(defn f [acc e]
+  (let [e (fmt-trans e)]
+    (merge-with union e acc)))
 
-(defn text->word-chain [text])
+(defn word-chain [word-transitions] (reduce f {} word-transitions))
+
+(defn word-chain-one-liner [word-transitions]
+  (apply merge-with union (map fmt-trans word-transitions)))
+
+(defn text->word-chain [text]
+  (word-chain (words->transitions (text->words text))))
